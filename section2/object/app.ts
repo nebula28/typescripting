7d36add5b1ee7608df599bfc1a8d8cfbd57ff243
Josh <joshua.nebeker@mx.com>
2021-10-04 14:46:56 -0600

    object types

---
+++ section2/object/app.ts
@@ -0,0 +1,29 @@
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'josh',
+    age: 30
+}
+
+console.log(person);
+
+const product = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+        title: 'Red Carpet',
+        description: 'A great carpet - almost brand-new!'
+    }
+}
+
+// {
+//     id: string;
+//     price: number;
+//     tags: string[];
+//     details: {
+//         title: string;
+//         description: string;
+//     }
+// }
