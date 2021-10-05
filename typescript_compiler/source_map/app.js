5591209cc62ac933a815b17b4ee573b58dbdf1dc
Josh <joshua.nebeker@mx.com>
2021-10-05 14:09:01 -0600

    sourcemap

---
+++ typescript_compiler/source_map/app.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('hell0 w0rld! s0u2c3 m4ps!');
+//# sourceMappingURL=app.js.map
---
+++ typescript_compiler/source_map/app.ts
@@ -0,0 +1,3 @@
+// set sourceMap: true and run tsc for .map files
+// these .map files can be viewed in the browser source tab
+console.log('hell0 w0rld! s0u2c3 m4ps!');
