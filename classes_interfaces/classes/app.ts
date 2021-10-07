b3e8a0c759f1ef60568b21984ea07d6f9147dcac
Josh <joshua.nebeker@mx.com>
2021-10-07 17:15:15 -0600

    classes

---
+++ classes_interfaces/classes/app.ts
@@ -0,0 +1,53 @@
+class Department {
+    // private readonly id: string;
+    // private name: string;
+    // public is default, private is not -> these are called 'modifiers'
+    private employees: string[] = [];
+
+    // constructor(id: string, name: string) {
+    //     this.id = id;
+    //     this.name = name;
+    // }
+
+    // shorthand contructor - readonly means the value cannot be altered
+    constructor(private readonly id: string, private name: string) {}
+
+    // you can pass keyword this to describe method to tell typescript 'this' refers only to an instance of the Department class
+    describe(this: Department) {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const marketing = new Department('d1', 'Marketing');
+// console.log(marketing);
+
+marketing.addEmployee('Josh');
+marketing.addEmployee('Jess');
+marketing.addEmployee('Bailey');
+
+marketing.describe();
+marketing.printEmployeeInfo();
+
+// not a good practice to allow object property to be publicly changed
+// marketing.employees[3] = 'public';
+
+// this code returns undefined as there is no name specified for the accounting object
+// typescript will throw an error because the type for this was set to Department
+// const accounting = { describe: marketing.describe};
+// accounting.describe();
+
+// this code works because the accounting object has a name property like a Department object would
+// const accounting = {
+//     name: 'accounting',
+//     describe: marketing.describe
+// }
+// accounting.describe();
---
+++ classes_interfaces/classes/app_finished.ts
@@ -0,0 +1,107 @@
+abstract class Department {
+    // static variables or methods cannot be access on instance of Department class i.e. using inside of describe method or it.fiscalYear
+    static fiscalYear = 2021;
+
+    // protected -> employees is available in classes which extend Department
+    protected employees: string[] = [];
+
+    constructor(protected readonly id: string, private name: string) {}
+
+    // to define abstract, the base class must also use abstract modifier
+    abstract describe(this: Department): void;
+
+    // static variables or methods can be used inside static methods
+    static createEmployee(name: string) {
+        // console.log(this.fiscalYear);
+        return { name: name };
+    }
+
+    // static methods or variables can be accessed in non static using the class name i.e. -> Department.fiscalYear
+    addEmployee(employee: string) {
+        // console.log(Department.fiscalYear);
+        this.employees.push(employee);
+    }
+
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+class IT extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, 'Information Technology');
+    }
+
+    describe() {
+        console.log('IT Department - ID: ' + this.id);
+    }
+}
+
+class Accounting extends Department {
+    private lastReport: string;
+
+    get latestReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No reports available...');
+    }
+
+    set latestReport(value: string) {
+        if (!value) {
+            throw new Error('set mostRecentReport: value is undefined...');
+        }
+        this.addReport(value);
+    }
+
+    describe() {
+        console.log('Accounting Department - ID: ' + this.id);
+    }
+
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+        this.lastReport = reports[0];
+    }
+
+    addReport(report: string) {
+        this.reports.push(report);
+        this.lastReport = report;
+    }
+
+    printReports() {
+        console.log(this.reports);
+    }
+
+    addEmployee(name: string) {
+        if(name === 'Josh') {
+            return;
+        }
+        this.employees.push(name);
+    }
+}
+
+const accounting = new Accounting('d3', []);
+
+accounting.latestReport = 'Reports Array';
+accounting.addReport('Second report');
+console.log(accounting.latestReport);
+accounting.addEmployee('Josh');
+accounting.addEmployee('Jess');
+
+// accounting describe method overrides Department abstract describe method
+accounting.describe();
+accounting.printReports();
+console.log(accounting);
+
+const it = new IT('d2', ['Kent']);
+
+it.addEmployee('Josh');
+
+const newEmployee = Department.createEmployee('Dee');
+console.log(`${Department.fiscalYear} Employee created: ${newEmployee.name}`);
+it.addEmployee(newEmployee.name);
+
+it.describe();
+it.printEmployeeInfo();
+console.log(it);
