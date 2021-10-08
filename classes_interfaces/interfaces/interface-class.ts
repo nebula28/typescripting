622c746947b21f10276f1509deed03260860bf0a
Josh <joshua.nebeker@mx.com>
2021-10-08 16:37:42 -0600

    interface as function

--- classes_interfaces/interfaces/interface-class.ts
+++ classes_interfaces/interfaces/interface-class.ts
@@ -1,6 +1,20 @@
-interface Greetable {
-    name: string;
+// type AddFn = (a: number, b: number) => number; this is more common than interface as functions
+// annonymous interface as a function
+interface AddFn {
+    (a: number, b: number): number;
+}
 
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+    return n1 + n2;
+}
+
+interface Named {
+    readonly name: string;
+}
+
+interface Greetable extends Named {
     greet(phrase: string): void;
 }
 
@@ -19,6 +33,8 @@ class Person implements Greetable {
 let user1: Greetable;
 
 user1 = new Person('Josh');
+// cannot change because readonly
+// user1.name = 'Jess';
 
 user1.greet('Hi there, my name is');
 console.log(user1);
