aa6f49613f4975d8eddc695b9d6bcba0c99d2ba5
Josh <joshua.nebeker@mx.com>
2021-10-12 16:02:10 -0600

    generic type with interface constraint

--- generics/constraint.ts
+++ generics/constraint.ts
@@ -4,3 +4,24 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({name: 'jess'}, {age: 32});
 console.log(mergedObj.age);
+
+interface Lengthy {
+    length: number;
+}
+
+// countAndDescribe returns type: tuple [T, string]
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let description = 'did not receive a value';
+    if (element.length === 1) {
+        description = 'received 1 value';
+    } else if (element.length > 1) {
+        description = 'received ' + element.length + ' values' ;
+    }
+    return [element, description];
+}
+
+console.log(countAndDescribe('Hi there')); // received 1 value 
+
+console.log(countAndDescribe(['Hello', 'Goodbye'])); // received # values
+
+console.log(countAndDescribe([])); // did not receive a value
