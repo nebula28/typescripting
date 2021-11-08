808647490d3ec97320de98e3f27f20df671cc795
Josh <joshua.nebeker@mx.com>
2021-11-08 16:57:19 -0700

    webpack

---
+++ webpack/project_1/src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+export function AutoBind(_target: any, _name: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+
+}
