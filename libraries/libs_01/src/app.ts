d022796179e347d0a7098da19f3892161035db95
Josh <joshua.nebeker@mx.com>
2021-12-20 16:49:43 -0700

    moar lodash

--- libraries/libs_01/src/app.ts
+++ libraries/libs_01/src/app.ts
@@ -1,8 +1,19 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-console.log(_.shuffle([1,2,3,4,5]));
+// console.log(_.shuffle([1,2,3,4,5]));
 
-// using declare
-declare var GLOBAL: any;
+// // using declare
+// declare var GLOBAL: any;
 
-console.log(GLOBAL);
+// console.log(GLOBAL);
+
+import { Product } from './product.model';
+const products = [{title: 'A carpet', price: 29.99}, {title: 'A book', price: 10.99}];
+const p1 = new Product('A book', 12.99);
+const loadedProducts = products.map(prod => {
+    return new Product(prod.title, prod.price);
+})
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
+console.log(p1.getInformation());
