b4ea54c3d576fe09b4d179591184d343d0d6a148
Josh <joshua.nebeker@mx.com>
2021-12-20 16:56:05 -0700

    plainToClass

--- libraries/libs_01/src/app.ts
+++ libraries/libs_01/src/app.ts
@@ -7,12 +7,16 @@
 
 // console.log(GLOBAL);
 
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 import { Product } from './product.model';
+
 const products = [{title: 'A carpet', price: 29.99}, {title: 'A book', price: 10.99}];
 const p1 = new Product('A book', 12.99);
-const loadedProducts = products.map(prod => {
-    return new Product(prod.title, prod.price);
-})
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price);
+// })
+const loadedProducts = plainToClass(Product, products);
 for (const prod of loadedProducts) {
     console.log(prod.getInformation());
 }
